171a717370ff028d1adf9bfe5f5210ff6bc165c4
geraldrolland <geraldrolland123@gmail.com>
2024-10-03 20:06:03 -0700

    pascal

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,6 +2,8 @@
 """
 0-main
 """
+
+
 def pascal_triangle(n):
     """
     Print the triangle
@@ -14,12 +16,12 @@ def pascal_triangle(n):
         return [[1], [1, 1]]
     all_list = [[1], [1, 1]]
     i = 2
-    while i < n :
+    while i < n:
         j = 0
         my_list = []
         while j < len(all_list[i-1]):
             if j == 0:
-                my_list.append(all_list[i-1][j])         
+                my_list.append(all_list[i-1][j])
             if j + 1 < len(all_list[i-1]):
                 sum = all_list[i-1][j] + all_list[i-1][j+1]
                 my_list.append(sum)
